6b0f645fb1d124f529b28b9bfedbba172d657418
Zuppr <zuppr@yahoo.co.uk>
2013-04-03 07:51:04 +0100

    rails-tm-04 1 commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTm04::Application.config.secret_token = 'd0883697315cb54de00b5b85a33bafac6999188293674f09bd6091573883af30b8f3c3d4990cec0d1fba368ab23e897ca55a5cad67315de33a92f5ef260318a9'
